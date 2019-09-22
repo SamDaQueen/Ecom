1d247e19c3668c69f47194eaa98c01416e84043b
SamDaQueen <samreenreyaz@outlook.com>
2019-09-22 19:45:30 +0530

    Carousel

--- app/src/main/java/com/android/ecom/Fragments/HomeFragment.java
+++ app/src/main/java/com/android/ecom/Fragments/HomeFragment.java
@@ -7,22 +7,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.android.ecom.R;
+import com.google.firebase.storage.StorageReference;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 import static com.android.ecom.Fragments.CartFragment.cart_list;
 
-
 public class HomeFragment extends Fragment {
+
+    CarouselView carouselView;
+    int NUMBER_OF_PAGES = 5;
+    StorageReference storageReference;
+    String[] images = {"image_1", "image_2", "image_3", "image_4", "image_5"};
+    int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
+//        storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://ecom-8af80.appspot.com");
+//        for (String image : images) {
+//            getDrawable(image);
+//        }
+        carouselView = root.findViewById(R.id.carouselView);
+        carouselView.setPageCount(NUMBER_OF_PAGES);
+        carouselView.setImageListener(imageListener);
         return root;
     }
 
@@ -34,4 +56,28 @@ public class HomeFragment extends Fragment {
         Objects.requireNonNull(getActivity()).setTitle("Home");
 
     }
+
+//
+//    public void getDrawable(String image) {
+////        storageReference.child("Carousel/image_1.jpeg").getDownloadUrl()
+////                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+////                    @Override
+////                  public void onSuccess(Uri uri) {
+////                      try {
+////                          InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
+////                          sampleImages = Arrays.copyOf(sampleImages, sampleImages.length + 1);
+////                          sampleImages[sampleImages.length - 1] = Integer.valueOf(uri);
+////                      } catch (FileNotFoundException e) {
+////                          image_1 = getResources().getDrawable(R.drawable.imageerror);
+////                      }
+////
+////                  }
+////              }
+////                ).addOnFailureListener(new OnFailureListener() {
+////            @Override
+////            public void onFailure(@NonNull Exception e) {
+////                Log.d(TAG, "onFailure: Carousel Image could not be loaded!");
+////            }
+////        });
+//    }
 }
